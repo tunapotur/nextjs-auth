1dcd2cebecf92c4676ce559cd7765a014ed39c73
Ahmet Tuna POTUR <tpotur@gmail.com>
2024-01-05 13:51:05 +0300

    simple fetch

--- app/register/page.jsx
+++ app/register/page.jsx
@@ -8,11 +8,43 @@ function Register() {
   const [password, setPassword] = useState("tunapotur41");
   const [loading, setLoading] = useState(false);
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!name || !email || !password) {
+      setError("All fields are necessary.");
+      return;
+    }
+
+    try {
+      const res = await fetch("api/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name,
+          email,
+          password,
+        }),
+      });
+
+      if (res.ok) {
+        const form = e.target;
+        form.reset();
+      } else {
+        console.log("User registration failed.");
+      }
+    } catch (error) {
+      console.log("Error during registration: ", error);
+    }
+  };
+
   return (
     <main className="main">
       <div className="form-container">
         <h2 className="form-header">Register</h2>
-        <form className="form">
+        <form className="form" onSubmit={handleSubmit}>
           <input
             className="form-input"
             type="text"
